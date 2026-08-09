e09b4c4ce73a773ee71f8ee17d9c8a212db2dabb
Empty[] <wefaj1234@gmail.com>
2026-08-09 22:47:54 +0700

    Add : Building feature 1
    -add prefab building
    -add building's script

---
+++ Assets/[00]Script/Building/BuildingEnum.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public enum BuildingName
+{
+    Home,
+    FisherpolkAquarium,
+    FishermanTackle,
+    Shipwright,
+    FishMerchant,
+    TheSeagullInn
+}
+
+public enum BuildingType
+{
+    Abandon,
+    Repaired,
+    MaxUpgrade
+}
---
+++ Assets/[00]Script/Building/Building_SO/BuildingStats.cs
@@ -0,0 +1,22 @@
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+namespace TableForge.Building
+{
+    [CreateAssetMenu(fileName = "BuildingStat", menuName = "Building_SO/Building Stats")]
+    public class BuildingStats : ScriptableObject
+    {
+        public BuildingName buildingName;
+        public BuildingType buildingType;
+        public int costToRepaired;
+        public int costToUpgrade_2;
+        public int costToUpgrade_3;
+
+        public GameObject buildingLV_1;
+        public GameObject buildingLV_2;
+        public GameObject buildingLV_3;
+
+    }
+    
+}
