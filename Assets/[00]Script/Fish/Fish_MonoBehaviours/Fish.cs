7cc67dd8dbe05ee418ec217a3d68575a2c952884
Empty[] <wefaj1234@gmail.com>
2026-08-13 11:26:52 +0700

    Update : Fishing system version prototype 2
    add:
    -fish prefabs and fish materials into folder "FishAssetTester"

--- Assets/[00]Script/Fish/Fish_MonoBehaviours/Fish.cs
+++ Assets/[00]Script/Fish/Fish_MonoBehaviours/Fish.cs
@@ -4,6 +4,7 @@ public class Fish : MonoBehaviour, IFishDataReceiver
 {
     public FishData data;
     public int weight;
+    private GameObject newPrefab;
 
     private void Start()
     {
@@ -20,10 +21,37 @@ public class Fish : MonoBehaviour, IFishDataReceiver
             return;
         }
 
+        newPrefab = data.Prefab;
         RollWeight();
+        ReplaceChildByIndex(0);
 
         Debug.Log($"Fish : {data.fishName}\nTier : {data.fishTier}\nWeight : {weight}\nID : {data.fishID}");
     }
+
+    public void ReplaceChildByIndex(int index)
+{
+    if (index < 0 || index >= transform.childCount)
+    {
+        Debug.LogWarning("Index incorrect");
+        return;
+    }
+
+    if (newPrefab == null)
+    {
+        Debug.LogWarning($"{name}: newPrefab is null");
+        return;
+    }
+
+    Transform child = transform.GetChild(index);
+    Vector3 pos = child.position;
+        Quaternion rot = child.rotation;
+        Transform parent = child.parent;
+        int siblingIndex = child.GetSiblingIndex();
+
+        Destroy(child.gameObject);
+        GameObject newChild = Instantiate(newPrefab, pos, rot, parent);
+        newChild.transform.SetSiblingIndex(siblingIndex);
+}
 
     private void RollWeight()
     {
